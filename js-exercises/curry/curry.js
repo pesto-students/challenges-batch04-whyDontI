fe1baaa855aa3d29d7cfe78212172199422615f7
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-11-02 10:06:20 +0530

    Remove unused validation

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,19 +1,4 @@
-function isFunction(functionToCheck) {
-  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
-}
-
-function isObject(variable) {
-  return Object.prototype.toString.call(variable) === '[object Object]';
-}
-
 function curry(func, obj) {
-  if (!isFunction(func)) {
-    throw new Error(`Expected first argument was Function, got ${typeof func}`);
-  }
-
-  if (!isObject(obj)) {
-    throw new Error(`Expected second argument was Object, got ${typeof obj}`);
-  }
   return function curried(...args) {
     if (args.length >= func.length) {
       return func.apply(obj, args);
