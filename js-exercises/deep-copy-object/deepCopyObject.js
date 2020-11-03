6bfc7a87343f587f0c231b5a0742969c5f7a8a16
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-11-03 15:39:41 +0530

    refactor deepCopyObject function

--- js-exercises/deep-copy-object/deepCopyObject.js
+++ js-exercises/deep-copy-object/deepCopyObject.js
@@ -1,3 +1,21 @@
-const deepCopyObject = objToCopy => JSON.parse(JSON.stringify(objToCopy));
+function deepCopyObject(inputObj) {
+  let value;
+
+  if (typeof inputObj !== 'object' || inputObj === null) {
+    return inputObj;
+  }
+
+  const outputObj = Array.isArray(inputObj) ? [] : {};
+
+  for (const key in inputObj) {
+    if (Object.prototype.hasOwnProperty.call(inputObj, key)) {
+      value = inputObj[key];
+
+      outputObj[key] = deepCopyObject(value);
+    }
+  }
+
+  return outputObj;
+}
 
 export { deepCopyObject };
